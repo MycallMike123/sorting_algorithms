5bed958b2537a866e93617f962f7bc657f711099
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-20 23:10:39 -0800

    The main header file for the project

--- sort.h
+++ sort.h
@@ -25,7 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void recursive_quick_sort(int *array, int low, int high, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
+void recursive_quick_sort(int *a, int *b);
+void lomuto_partition(int *array, size_t size, int left, int right);
+int _lomuto_partition(int *array, size_t size, int left, int right);
 
 #endif
